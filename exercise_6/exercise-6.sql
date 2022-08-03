214ca40a7ff3a152c056d23c1d86a59e1da97028
Aphenphos <MaxwillWinters@gmail.com>
2022-08-03 19:21:11 -0400

    4 and 5 done

--- exercise_6/exercise-6.sql
+++ exercise_6/exercise-6.sql
@@ -1,2 +1,9 @@
 -- find all film titles rented out by customers with the first_name Roberta
 -- Similar to exercise 5
+select film.title
+from film
+left join film_actor
+    on film_actor.film_id = film.film_id
+left join actor
+    on film_actor.actor_id = actor.actor_id
+    where customer.first_name = 'Roberta'
